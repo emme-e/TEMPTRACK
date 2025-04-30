57bd8f817119b87648129af3ac17cae87c6fb2f8
emme-e <sabriel.earl@gmail.com>
2025-04-30 12:04:33 +0100

    Update and rename newtons_law_of_cooling.py to ClassNewtons.py

--- newtons_law_of_cooling.py
+++
@@ -1,8 +0,0 @@
-def newtons_law_of_cooling(initial_temp, enviroment_temp, h, area, dt, c, m):
-    """
-    Returns the new temperature of the object after time dt using Newton's Law of Cooling.
-    ... need to make some parameters... follow the video
-    """
-    q = h * area * (enviroment_temp - initial_temp)
-    delta_T = (q * dt) / (m * c)
-    return initial_temp + delta_T
