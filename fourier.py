564390a77258fb8dc70bdfac57c8af1551cee262
emme-e <sabriel.earl@gmail.com>
2025-05-03 15:46:31 +0100

    Update fourier.py

--- fourier.py
+++ fourier.py
@@ -1,24 +1,21 @@
-import sympy as sym
-'''
-added another heat modelling to solids to extend our library
-q - heat transfer rate (in watts)
-k - stands for the thermal conductivity
-A - this equates for the cross sectional area of the solid
-T(x) - temperature
-dT/dx - temperature gradient. rate fo change of temperature
+def fourier_law(k ,A , dT_dx):
+    """
+    This function implements Fourier's Law of Heat Conduction.
+    It calculates the heat transfer rate (q) through a solid material.
 
-'''
+    Fourier's Law states that the heat transfer rate (q) is proportional to the negative gradient of temperature (dT/dx)
+    and the area (A) through which the heat is being transferred.
 
-k, A, x, q= sym.symbols("k A x q")
-T = sym.Function("T")(x)  
+    The formula is given by:
+    q = -k * A * (dT/dx)
 
-k_value = float(input("Enter thermal conductivity k (W/m . k): "))
-A_value = float(input("Enter the cross-sectional area A (m^2): "))
-dTdx_value = float(input("Enter temperature gradient dT/dx (K/m): "))
-"""
-allows the use to now input values into the equation to workout the heat transfer rate of the object there testing
-"""
-q_expression = -k * A * sym.diff(T, x)
-q_value = q_expression.subs({k: k_value, A: A_value, sym.diff(T, x): dTdx_value})
-q_value_numeric = sym.N(q_value)
-print(f" The Heat transfer rate of q = {q_value_numeric} watts")
+    parameters:
+    k = (float) thermal conductivity (W/m·K)
+    A = (float) cross-sectional area (m^2)
+    dT/dx = (float) temperature gradient (K/m)
+
+    Returns:
+    - q (float): heat transfer rate (W) 
+    """
+    q = -k * A * dT_dx 
+    return round(q,2), f" The Heat transfer rate of q = {q} watts"
