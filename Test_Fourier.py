a79e333f93cae46101e5febf189f7e60245161c6
JAMtheMan2004 <jajbaum@gmail.com>
2025-05-05 10:13:36 +0100

    Update Test_Fourier.py

--- Test_Fourier.py
+++ Test_Fourier.py
@@ -8,17 +8,19 @@ def test_fourier_law(k ,A , dT_dx):
         A (float): this represents the cross sectional area of the solid we are testing
         dT_dx (float): this is the temperature gradient measuring the rate of heat transfer
     """
-    k = 50
-    A = 100
-    dT_dx = 3
-
     expected_q = -k * A * dT_dx
     result_q, _ = fourier_law(k, A, dT_dx)
     assert math.isclose(expected_q, result_q, rel_tol=1e-5), f"Expected {expected_q}, but got {result_q}"
     print("test_fourier_law passed")
    
-
-if __name__ == "__main__":
-    test_fourier_law(50,100,3)
+while True:
+    try:
+        k = float(input("Enter thermal conductivity (k): "))
+        A = float(input("Enter cross-sectional area (A): "))
+        dT_dx = float(input("Enter temperature gradient (dT/dx): "))
+        test_fourier_law(k, A, dT_dx)
+        break  
+    except ValueError: 
+        print ("Please enter the appropriate numbers")
 
 
